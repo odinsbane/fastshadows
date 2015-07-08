20c61f2a99b9c35d9e326bd818521784bb264b3f
Matt Smith <melkor@orangepalantir.org>
2015-07-08 21:25:30 +0100

    started filling out the geometry file.

---
+++ src/cpp/Geometry.cpp
@@ -0,0 +1,10 @@
+#include "Geometry.h"
+
+/**
+ * Each index represents a vertex. Each triangle is composed of
+ * 3 vertexs. 
+ * */
+void findAdjacencies(int* indexes, std::vector<Adjacent>& target){
+    
+}
+
--- src/cpp/Geometry.h
+++ src/cpp/Geometry.h
@@ -1,4 +1,20 @@
 #ifndef __Shadow_Volumes__Geometry__
 #define __Shadow_Volumes__Geometry__
+#include <vector>
 
+/**
+ * The triangle value determines where in the indexes array the triangle
+ * can be found edgeA and edgeB are the indexes of the vertex found
+ * in the indexes array. adjacent is the index of the remaining vertex
+ * in the triangle and adjacentTriangle is the index of the triangle.
+ * 
+ * */
+class Adjacent{
+    public:
+        int triangle;
+        int edgeA,edgeB;
+        int adjacent;
+        int adjacentTriangle; 
+};
+void findAdjacencies(int* indexes, std::vector<Adjacent>& target);
 #endif
